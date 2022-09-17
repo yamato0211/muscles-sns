93d25f47fec5ec02289402fbb141dc384d70701f
yamato0211 <kumayama0211@gmail.com>
2022-09-17 20:42:41 +0900

    [change] main.pyの修正

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from db.models import Base
+from db.database import engine
+from routers.main import router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="muscles-backend"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def hello():
     return {"message" : "Hello world!"}
+
+app.include_router(router, prefix="/api/v1")
