0660fc95d513bdfcaadaae8145887765017f1b26
yamato0211 <kumayama0211@gmail.com>
2022-09-18 19:18:11 +0900

    [change] heroku設定

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from db.models import Base
 from db.database import engine
 from routers.main import router
-import uvicorn
+
 
 Base.metadata.create_all(bind=engine)
-PORT = os.environ.get("PORT",8000)
+
 
 app = FastAPI(
     title="muscles-backend"
@@ -27,6 +26,3 @@ async def health():
 
 app.include_router(router, prefix="/api/v1")
 
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0",port=PORT)
