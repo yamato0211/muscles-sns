8164b864fcdc28a19f14e851033ae20c31b0986e
yamato0211 <kumayama0211@gmail.com>
2022-09-20 21:06:23 +0900

    [change] PostSchema修正

--- schemas/posts.py
+++ schemas/posts.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import List
 from .users import User
 from pydantic import BaseModel, Field
+from .comments import Comment
 
 class PostPayload(BaseModel):
     content: str = Field(default=None, title="Thie is a TweetContent", max_length=120)
@@ -13,6 +14,7 @@ class Post(BaseModel):
     created_at: datetime = None
     user: User
     favorites: List[User]
+    comments: List[Comment]
 
     class Config:
         orm_mode = True
