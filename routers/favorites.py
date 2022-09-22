aba7332cbc16673829a4eebb2b75ebc8205a43b3
yamato0211 <kumayama0211@gmail.com>
2022-09-23 00:12:57 +0900

    [add]commentいいね機能の追加(router)

--- routers/favorites.py
+++ routers/favorites.py
@@ -2,9 +2,11 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm.session import Session
 from schemas.favorites import Favorites as FavoriteSchema
+from schemas.favorites import CommentFavorites as Comment
 from db.database import get_db
 from utils.jwt import get_current_user
-from cruds.favorites import create_favorites_by_ids, delete_favorites_by_ids
+from cruds.favorites import create_post_favorites_by_ids, delete_post_favorites_by_ids
+from cruds.favorites import create_comment_favorites_by_ids,delete_comment_favorites_by_ids
 
 favorite_router = APIRouter()
 
@@ -13,12 +15,26 @@ favorite_router = APIRouter()
 async def create_favorite(post_id: str, db: Session = Depends(get_db), user_id: str = Depends(get_current_user)):
     if user_id is None:
         raise HTTPException(status_code=403, detail="jwt_token is invalid")
-    favorite = create_favorites_by_ids(db, post_id, user_id)
+    favorite = create_post_favorites_by_ids(db, post_id, user_id)
     return favorite
 
 @favorite_router.delete("/{post_id}")
 async def delete_favorite(post_id: str, db: Session = Depends(get_db), user_id: str = Depends(get_current_user)):
     if user_id is None:
         raise HTTPException(status_code=403, detail="jwt_token is invalid")
-    delete_favorites_by_ids(db, post_id, user_id)
+    delete_post_favorites_by_ids(db, post_id, user_id)
     return {"detail" : "OK!!"}
+
+@favorite_router.post("/comment/{comment_id}", response_model=Comment)
+async def create_comment_favorite(comment_id: str, db: Session = Depends(get_db), user_id: str = Depends(get_current_user)):
+    if user_id is None:
+        raise HTTPException(status_code=403, detail="jwt_token is invalid")
+    favorite = create_comment_favorites_by_ids(db, comment_id, user_id)
+    return favorite
+
+@favorite_router.delete("/comment/{comment_id}")
+async def delete_comment_favorite(comment_id: str, db: Session = Depends(get_db), user_id: str = Depends(get_current_user)):
+    if user_id is None:
+        raise HTTPException(status_code=403, detail="jwt_token is invalid")
+    delete_comment_favorites_by_ids(db, comment_id, user_id)
+    return {"detail" : "OK"}
